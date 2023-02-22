192364a7e01a5f20d9e347d6d7507c30d339ca33
rshinkai <ryo.shinkai1991@gmail.com>
2023-02-22 19:39:42 +0900

    Add user create feature

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,20 @@ class Api::V1::UsersController < ApplicationController
         # params[:id] comes from path param like api/:id
         render json: User.find(params[:id])
     end
+
+    # POST /users
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end 
+    end
+
+    # define user params
+    private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
     # TODO: list resources here
-      resources :users, only: [:show]
+      resources :users, only: %i[show create]
     end
   end
 end
