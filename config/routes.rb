e7909e8ef3f19153725cca429a0cad77e89cbccc
rshinkai <ryo.shinkai1991@gmail.com>
2023-02-22 19:51:37 +0900

    Add user delete feature

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
     # TODO: list resources here
-      resources :users, only: %i[show create update]
+      resources :users, only: %i[show create update destroy]
     end
   end
 end
