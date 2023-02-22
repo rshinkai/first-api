f575ba09f7af49ed1085daf54065527de798bba3
rshinkai <ryo.shinkai1991@gmail.com>
2023-02-22 18:44:47 +0900

    Fix User migration script

--- db/migrate/20230222094153_create_users.rb
+++ db/migrate/20230222094153_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :email, null: false
-      t.string :email, unique: true
+      t.index :email, unique: true
       t.string :password, null: false
 
       t.timestamps
