444808fdb428f5a56e9510b9bd532b7fe9670fdd
rshinkai <ryo.shinkai1991@gmail.com>
2023-02-22 18:48:32 +0900

    Add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    # Add validation
+    validates :email, uniqueness: true
+    validates_format_of :email, with:/@/
+    # presence checks not empty
+    validates :password, presence: true
 end
