1af8da1f88e2e7f83b495c42316832b2d7f09a1b
rshinkai <ryo.shinkai1991@gmail.com>
2023-02-23 22:19:08 +0900

    Add Order model

---
+++ app/models/order.rb
@@ -0,0 +1,6 @@
+class Order < ApplicationRecord
+  validates :total, numericality: { greater_than_or_equal_to: 0}
+  validates :total, presence: true
+  
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_052143) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_131317) do
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.decimal "price"
@@ -29,5 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_052143) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
 end
