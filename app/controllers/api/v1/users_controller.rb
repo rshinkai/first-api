2e6a0e38df4de844e41dbfdf8bcba4509435f357
rshinkai <ryo.shinkai1991@gmail.com>
2023-02-23 20:13:41 +0900

    Add user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,14 +6,14 @@ class Api::V1::UsersController < ApplicationController
     def show
         # response format is JSON
         # params[:id] comes from path param like api/:id
-        render json: User.find(params[:id])
+        render json: UserSerializer.new(@user).serializable_hash.to_json
     end
 
     # POST /users
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user, status: :created
+            render json: UserSerializer.new(@user).serializable_hash.to_json, status: :created
         else
             render json: @user.errors, status: :unprocessable_entity
         end 
@@ -22,7 +22,7 @@ class Api::V1::UsersController < ApplicationController
     # PATCH/PUT /users/:id
     def update
         if @user.update(user_params)
-            render json: @user, status: :ok
+            render json: UserSerializer.new(@user).serializable_hash.to_json
         else
             render json: @user.errors, status: :unprocessable_entity
         end
