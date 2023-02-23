9d7070f41ac68e44ad3bb0f24446e2fdcba57944
rshinkai <ryo.shinkai1991@gmail.com>
2023-02-23 21:55:12 +0900

    Add filter by title

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0}, presence: true
   belongs_to :user
+
+  # scope: A mechanism that allows common conditional expressions (query processing) to be defined with a name on the model side and called like methods with that name.
+  scope :filter_by_title, lambda { |keyword|where('lower(title) LIKE ?', "%#{keyword.downcase}%")}
 end
