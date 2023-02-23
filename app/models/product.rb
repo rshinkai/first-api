df30cdaa2f2065942d5fef64690df0eef8fd334d
rshinkai <ryo.shinkai1991@gmail.com>
2023-02-23 22:10:56 +0900

    Add search product feature

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,15 @@ class Product < ApplicationRecord
   scope :below_or_equal_to_price, lambda { |price|where('price <= ?', price)}
   # sort
   scope :recent, lambda {order(:updated_at)}
+
+  # return the products as an object ActiveRecord::Relation to chain other methods
+  def self.search(params = {})
+    products = params[:product_ids].present? ? Product.where(id:params[:product_ids]) : Product.all
+    products = products.filter_by_title(params[:keyword]) if params[:keyword]
+    products = products.above_or_equal_to_price(params[:min_price].to_f) if params[:min_price]
+    products = products.below_or_equal_to_price(params[:max_price].to_f) if params[:max_price]
+    products = products.recent if params[:recent]
+    
+    products
+  end
 end
