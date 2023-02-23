a0d012221114810e4c3c172e6096e3871dbb109b
rshinkai <ryo.shinkai1991@gmail.com>
2023-02-24 01:51:18 +0900

    Decrement product quantity

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -2,4 +2,10 @@ class Placement < ApplicationRecord
   belongs_to :order
   # Bidirectional data referencing by inverse_of
   belongs_to :product, inverse_of: :placements
+
+  # after_create is executed just before commit to DB
+  after_create :decrement_product_quantity!
+  def decrement_product_quantity!
+    product.decrement!(:quantity, quantity)
+  end
 end
